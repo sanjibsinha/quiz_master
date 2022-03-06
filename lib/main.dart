842cc6fc1269134c176668b5a259c71119150ade
Sanjib Sinha <sanjib12sinha@gmail.com>
2022-03-06 06:02:22 +0530

    Initial

--- lib/main.dart
+++ lib/main.dart
@@ -3,76 +3,34 @@ import 'package:google_fonts/google_fonts.dart';
 import 'package:rflutter_alert/rflutter_alert.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const Mathematico());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+class Mathematico extends StatelessWidget {
+  const Mathematico({Key? key}) : super(key: key);
 
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'First Quiz',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(),
+      theme: ThemeData(primarySwatch: Colors.orange),
+      home: const MathematicoHome(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key}) : super(key: key);
+class MathematicoHome extends StatefulWidget {
+  const MathematicoHome({Key? key}) : super(key: key);
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<MathematicoHome> createState() => _MathematicoHomeState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  List<String> questions = [
-    '29 + 5 = 34',
-    '71 - 4 = 66',
-    '51 - 9 = 41',
-    '78 + 6 = 84',
-  ];
-
-  int index = 0;
-
-  List<bool> answers = [
-    true,
-    false,
-    false,
-    true,
-  ];
+class _MathematicoHomeState extends State<MathematicoHome> {
+  Mathematico mathematico = const Mathematico();
 
   List<Text> check = [];
-
-  void checkAnswer(bool userAnswered) {
-    bool correctAnswer = answers[index];
-
-    if (correctAnswer == userAnswered) {
-      check.add(
-        const Text(
-          'You\'re Correct.',
-          style: TextStyle(
-            fontSize: 20.0,
-            fontWeight: FontWeight.bold,
-          ),
-        ),
-      );
-    } else {
-      check.add(
-        const Text(
-          'You\'re Wrong',
-          style: TextStyle(
-            fontSize: 20.0,
-            fontWeight: FontWeight.bold,
-          ),
-        ),
-      );
-    }
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -99,7 +57,7 @@ class _MyHomePageState extends State<MyHomePage> {
                 margin: const EdgeInsets.fromLTRB(20.0, 5.0, 20.0, 5.0),
                 alignment: Alignment.center,
                 child: Text(
-                  questions[index],
+                  '',
                   style: GoogleFonts.lalezar(
                     textStyle: const TextStyle(
                       fontSize: 30.0,
@@ -131,20 +89,7 @@ class _MyHomePageState extends State<MyHomePage> {
       padding: const EdgeInsets.all(5.0),
       child: ElevatedButton(
         onPressed: () {
-          setState(() {
-            checkAnswer(corerctOrWrong);
-            index++;
-            if (index >= answers.length) {
-              index = 0;
-
-              Alert(
-                context: context,
-                title: "FirstQuiz Completed",
-                desc: "Flutter and You, both are awesome.",
-              ).show();
-              check.clear();
-            }
-          });
+          setState(() {});
         },
         child: Text(
           answer,
