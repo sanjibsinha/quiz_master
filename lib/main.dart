47a85cd2bbb31ddf5b3ab3167066183e3ed92646
Sanjib Sinha <sanjib12sinha@gmail.com>
2022-03-08 16:01:13 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -188,16 +188,23 @@ class PaddingCustom extends StatelessWidget {
  void main() {
   
   AnImmutable anImmutable = AnImmutable(2); // setting a value which was final
+  AnImmutable anotherImmutable = AnImmutable(4);
+  
+  
+  //AnImmutable.mycConstant = 'Anything else'; // error
+  // Constant variables can't be assigned a value.
   
   print(AnImmutable.mycConstant); 
   
   print(anImmutable.myFinal); // 2
+  print(anotherImmutable.myFinal); //4
   
   print(anImmutable.myDate);
   
   /**
    A Constant String
    2
+   4
    2022-03-08 08:20:13.398 
   */
   
